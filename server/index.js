8bd39fd2d26ecd3720a9fdfce6ed4ff14cc19a38
Alan Phillip <you@example.com>
2020-04-28 02:24:12 -0400

    added a way to track users in chat

--- server/index.js
+++ server/index.js
@@ -21,9 +21,12 @@ io.on('connection', (socket) => {
         if(error) return callback(error);
 
         socket.emit('message', {user:'admin', text: `${user.name}, welcome to the room ${user.room}.`});
-        socket.broadcast.to(user.room).emit('message', {user:'admin', text:`${user.name} has joined!`})
+        socket.broadcast.to(user.room).emit('message', {user:'admin', text:`${user.name} has joined!`});
 
         socket.join(user.room);
+
+        io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
+
         callback();
     })
 
@@ -32,13 +35,15 @@ io.on('connection', (socket) => {
         
 
         io.to(user.room).emit('message', {user:user.name, text:message});
+        io.to(user.room).emit('roomData', {room:user.room, users: getUsersInRoom(user.room)});
 
         callback(); 
     })
 
 
     socket.on('disconnect', () => {
-        console.log('user has left');
+        const user = removeUser(socket.id);
+        io.to(user.room).emit('message', {user: 'admin', text:`${user.name} has left.`});
     })
 });
 
